670e1b39e6b3bca1f9997ff7d707766c10152a0c
itdistrigama-byte <itdistrigama@gmail.com>
2026-06-10 17:42:55 -0400

    v6.0 catálogo ERP + fotos ecommerce + bugfixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'distrigama-v4';
+const CACHE = 'distrigama-v5';
 const OFFLINE_ASSETS = ['/', '/index.html', '/manifest.json', '/data-catalog.js',
   '/js/config.js', '/js/utils.js', '/js/auth.js', '/js/censo.js',
   '/js/cartera.js', '/js/pdf-xlsx.js', '/js/reporte.js'];
